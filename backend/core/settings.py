41da29c897d22adb7866dae43e5783c9b82163dc
Raylin0722 <107912521+Raylin0722@users.noreply.github.com>
2026-04-29 09:13:40 +0800

    create SQLite Setting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,3 +1,16 @@
+import os
+
+# 檢查基本資料
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+if not SECRET_KEY:
+    raise RuntimeError("無法讀取 DJANGO_SECRET_KEY，請檢查 entrypoint.sh")
+
+
+
 INSTALLED_APPS = [
     'daphne',  # 必須放在第一個
     'django.contrib.admin',
@@ -14,11 +27,16 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True # 開發初期先全開
 ASGI_APPLICATION = 'core.asgi.application'
+REDIS_URL = os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/0')
 
 # 這裡先用記憶體模擬 Redis，方便你沒開 Docker 也能跑，
 # 但正式開發建議改用 redis://redis:6379
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        # "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [REDIS_URL],
+        },
     },
 }
